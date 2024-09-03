9b2e6977af03ddb15d67d13bfdc439d45a18ff35
Burak Karaman <karaman.buraak@gmail.com>
2024-09-03 22:46:10 +0200

    initial commit & add shadcn components

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bri">
+      <Button variant="destructive" size="icon">Deneme</Button>
+      <button>test</button>
+    </div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        mydark1: "#1A1A1A",
+        mydark2: "#333333",
+        myLight1: "#E09F3E",
+        myLight2: "#FFD580",
+      },
+      brightness: {
+        35: ".35",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+export default config;
