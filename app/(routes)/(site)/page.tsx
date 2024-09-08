4f9f305bf66f7f8af764f4c7d43d4f8ea4d05e6c
Burak Karaman <karaman.buraak@gmail.com>
2024-09-08 11:26:46 +0200

    Update page.tsx, layout.tsx, and globals.css files; add ModeToggle component, Navbar component, and ThemeProvider; install next-themes package

--- app/(routes)/(site)/page.tsx
+++ app/(routes)/(site)/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
-      <Button variant="destructive" size="icon">Deneme</Button>
-      <button>test</button>
     </div>
   );
 }
---
+++ app/(routes)/_components/UserToggle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserToggle = () => {
+    return <div>UserToggle</div>;
+};
+
+export default UserToggle;
